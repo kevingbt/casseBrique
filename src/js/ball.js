44058261c69cc9a2a5cc87bf561ff287d7fcfcc1
Mohamed-Amine <mmejjiou@gmail.com>
2025-11-21 10:12:49 +0100

    comenter le code

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,28 +1,36 @@
+/**
+ * @class
+ *
+ * @description
+ * Cette classe permet de créer une balle
+ *
+ * @param {number} x - la position des absis.
+ * @param {number} y - la position des ordonnées
+ * @param {number} radius - le rayon de la balle.
+ * @param {string} [color="blue"] - la couleur de la balle.
+ * @param {canvas} ctx - dessin va etre en 2D 
+ */
 class Ball {
+    
     constructor(x, y, radius, color = "blue") {
-        this.x = x;             // Position en X
-        this.y = y;             // Position en Y
-        this.radius = radius;   // Rayon de la balle
-        this.color = color;     // Couleur de remplissage
+        this.x = x;             // Position de X
+        this.y = y;             // Position de Y
+        this.radius = radius;   // Rayon
+        this.color = color;     // Couleur
     }
-
-    // ctx = canvas.getContext("2d")
     draw(ctx) {
-        ctx.beginPath(); // je commence le dessin
-
-        ctx.fillStyle = this.color; // je defini la couleur de remplissage
-
+        ctx.beginPath();               // Début d dessin
+        ctx.fillStyle = this.color;    // on choisi la couleur qu'on a defini avant
         ctx.arc(
-            this.x,          // Coordonnée de X
-            this.y,          // Coordonnée  de Y
-            this.radius,     // le Rayon
-            0,               // Début de l'arc
-            Math.PI * 2      // Fin de l'arc
+            this.x,                    // Coordonnée de X
+            this.y,                    // Coordonnée de Y
+            this.radius,               // le Rayon
+            0,                         // Début de l'angle
+            Math.PI * 2                // Fin de l'angle 
         );
-
-
-        ctx.fill();                 // Remplit le cercle
-
-        ctx.closePath(); // Fin du chemin de dessin
+        ctx.fill();                    // Remplissage du cercle
+        ctx.closePath();               // Fin du dessin
     }
 }
+
+export default Ball;
