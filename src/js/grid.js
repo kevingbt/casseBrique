5377804bee10cacaaa08c70326f9393d5bef8684
Jules-95 <jlsmillet@yahoo.fr>
2025-11-21 11:07:39 +0100

    grille

---
+++ src/js/grid.js
@@ -0,0 +1,37 @@
+ class Grid {
+  constructor(columnCount, rowCount, width, height, padding, offsetTop, offsetLeft) {
+    this.columnCount = columnCount;
+    this.rowCount = rowCount;
+    this.padding = padding;
+    this.offsetTop = offsetTop;
+    this.offsetLeft = offsetLeft;
+
+    this.width = width;
+    this.height = height;
+    this.color = "#0095DD";
+
+    this.bricks = [];
+    this.initializeBricks();
+  }
+
+  initializeBricks() {
+    for (let c = 0; c < this.columnCount; c++) {
+      this.bricks[c] = [];
+      for (let r = 0; r < this.rowCount; r++) {
+        const brickX = (r * (this.width + this.padding)) + this.offsetLeft;
+        const brickY = (c * (this.height + this.padding)) + this.offsetTop;
+
+        this.bricks[c][r] = {
+          x: brickX,
+          y: brickY,
+          width: this.width,
+          height: this.height,
+          status: 1,
+          color: this.color
+        };
+      }
+    }
+  }
+}
+
+export default Grid
