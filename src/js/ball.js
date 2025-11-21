4ee19ebe2d9c23807172d734d55c02ae5114156d
Mohamed-Amine <mmejjiou@gmail.com>
2025-11-21 12:08:23 +0100

    rajout de ball #5

--- src/js/ball.js
+++ src/js/ball.js
@@ -2,34 +2,68 @@
  * @class
  *
  * @description
- * Cette classe permet de créer une balle
+ * Cette classe permet de créer une balle avec dessin, mouvement et collisions.
  *
- * @param {number} x - la position des absis.
- * @param {number} y - la position des ordonnées
- * @param {number} radius - le rayon de la balle.
- * @param {string} [color="blue"] - la couleur de la balle.
- * @param {canvas} ctx - dessin va etre en 2D 
+ * @param {number} x - Position X.
+ * @param {number} y - Position Y.
+ * @param {number} radius - Rayon de la balle.
+ * @param {string} [color="blue"] - Couleur.
  */
 class Ball {
     
     constructor(x, y, radius, color = "blue") {
-        this.x = x;             // Position de X
-        this.y = y;             // Position de Y
-        this.radius = radius;   // Rayon
-        this.color = color;     // Couleur
+        this.x = x;             
+        this.y = y;             
+        this.radius = radius;   
+        this.color = color;     
+
+        // Vitesses par défaut
+        this.dx = 2;
+        this.dy = -2;
     }
+
     draw(ctx) {
-        ctx.beginPath();               // Début d dessin
-        ctx.fillStyle = this.color;    // on choisi la couleur qu'on a defini avant
-        ctx.arc(
-            this.x,                    // Coordonnée de X
-            this.y,                    // Coordonnée de Y
-            this.radius,               // le Rayon
-            0,                         // Début de l'angle
-            Math.PI * 2                // Fin de l'angle 
-        );
-        ctx.fill();                    // Remplissage du cercle
-        ctx.closePath();               // Fin du dessin
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    //  Mouvement et Collisions de la Balle
+    move(canvas, livesObj ,paddle ) {
+
+        // quand sa touche les murs sur les cotés
+        if (this.x + this.dx > canvas.width - this.radius || this.x + this.dx < this.radius) {
+            this.dx = -this.dx;
+        }
+
+        // quand sa touche le mur du haut
+        if (this.y + this.dy < this.radius) {
+            this.dy = -this.dy;
+        }
+
+        // quand sa touche le sol
+        else if (this.y + this.dy > canvas.height - this.radius) {
+            livesObj.lives--;
+
+            if (livesObj.lives <= 0) {
+                this.x = canvas.width / 2;
+                this.y = canvas.height - 30;
+                this.dx = 2;
+                this.dy = -2;
+                paddle.x = (canvas.width - paddle.width) / 2;                
+            } 
+            else {
+                this.x = canvas.width / 2;
+                this.y = canvas.height - 30;
+                this.dx = 2;
+                this.dy = -2;
+                paddle.x = (canvas.width - paddle.width) / 2;
+            }
+        }
+        this.x += this.dx;
+        this.y += this.dy;
     }
 }
 
