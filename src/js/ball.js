2a7952511434c06c08922d57cd1391144a11a205
Mohamed-Amine <mmejjiou@gmail.com>
2025-11-21 09:06:05 +0100

    comentation du code

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,22 +1,28 @@
-    class Ball {
-        constructor(x, y, radius, color = "blue") {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
+class Ball {
+    constructor(x, y, radius, color = "blue") {
+        this.x = x;             // Position en X
+        this.y = y;             // Position en Y
+        this.radius = radius;   // Rayon de la balle
+        this.color = color;     // Couleur de remplissage
     }
-// ctx est le context du canvas "var ctx = canvas.getContext("2d");""
+
+    // ctx = canvas.getContext("2d")
     draw(ctx) {
-        //la je commence le dessin
-        ctx.beginPath();
-        //icic this.x correspond a l'ax des abscisses et this.y a l'axe des ordonnees this.radius est le rayon de la balle 0 correspond au debut de l'arc et Math.PI * 2 correspond a la fin de l'arc
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        //ctx.fill() permet de remplir la balle
-        
-        ctx.fill();
-        //ctx.fillStyle c'est la couleur de la balle
-       //ctx.closePath();  je termine le dessin
-        ctx.closePath();
+        ctx.beginPath(); // je commence le dessin
+
+        ctx.fillStyle = this.color; // je defini la couleur de remplissage
+
+        ctx.arc(
+            this.x,          // Coordonnée de X
+            this.y,          // Coordonnée  de Y
+            this.radius,     // le Rayon
+            0,               // Début de l'arc
+            Math.PI * 2      // Fin de l'arc
+        );
+
+
+        ctx.fill();                 // Remplit le cercle
+
+        ctx.closePath(); // Fin du chemin de dessin
     }
-    }
+}
