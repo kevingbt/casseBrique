f36c3cc9b5b8c418a8f05af16e9b25314e81b31d
Mohamed-Amine <mmejjiou@gmail.com>
2025-11-21 08:59:04 +0100

    fix : coloration de la ball

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,5 +1,5 @@
     class Ball {
-        constructor(x, y, radius, color = "#0051ffff") {
+        constructor(x, y, radius, color = "blue") {
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -10,12 +10,13 @@
         //la je commence le dessin
         ctx.beginPath();
         //icic this.x correspond a l'ax des abscisses et this.y a l'axe des ordonnees this.radius est le rayon de la balle 0 correspond au debut de l'arc et Math.PI * 2 correspond a la fin de l'arc
+        ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         //ctx.fill() permet de remplir la balle
+        
         ctx.fill();
         //ctx.fillStyle c'est la couleur de la balle
-        ctx.fillStyle = this.color;
-        //ctx.closePath();  je termine le dessin
+       //ctx.closePath();  je termine le dessin
         ctx.closePath();
     }
     }
