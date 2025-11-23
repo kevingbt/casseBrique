f76c27f46b3d30e08476b7d65fb63f7a9dabb854
lluc <leger.luc@gmail.com>
2025-11-23 19:24:27 +0100

    fix:  activer un point d'entrée pour l'application #13

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+import GameUI from "./gameUI.js";
+import Grid from "./grid.js";
+
+/**
+ * Point d'entrée principal du jeu Casse-Brique
+ * Initialise le canvas et lance la boucle de jeu
+ */
+
+// Configuration du canvas
+const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvaGame"));
+const ctx = canvas.getContext("2d");
+
+// Taille du canvas
+canvas.width = 600;
+canvas.height = 400;
+
+// Initialisation du jeu
+const gameUI = new GameUI(canvas, 3);
+
+// Initialisation de la grille de briques
+const grid = new Grid(
+  5,      // columnCount
+  3,      // rowCount
+  75,     // width
+  20,     // height
+  10,     // padding
+  30,     // offsetTop
+  canvas.width
+);
+
+/**
+ * Boucle principale du jeu
+ */
+function gameLoop() {
+  // Nettoyer le canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Dessiner les éléents du jeu
+  gameUI.draw();
+
+
+  // Continuer la boucle
+  requestAnimationFrame(gameLoop);
+}
+
+
+// D�marrer le jeu
+gameLoop();
